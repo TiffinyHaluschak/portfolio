2d6ca141106e14a33fe9a45f07bb78b80af67c28
TiffinyHaluschak <tiffiny.haluschak@gmail.com>
2024-09-11 10:44:33 -0400

    Debugging contact form. Created a new 'public' directory.

--- server/server.js
+++ server/server.js
@@ -1,14 +1,19 @@
+// Import required modules
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+// Create an instance of the Express app, which will be used to set up routes, middleware, and server behavior
 const app = express();
 
-// Body parser middleware to handle form data
+// Set up body parser middleware to handle form data
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Serve static files (like my front-end HTML/CSS)
-app.use(express.static('public')); // Assuming my front-end is in 'public'
+// This line tells the Express app to serve static files 
+// (such as HTML, CSS, and client-side JavaScript) from 
+// the public directory.
+app.use(express.static('public')); // Assumes my front-end files are stored in 'public'
 
 // Contact form route
 app.post('/send', (req, res) => {
